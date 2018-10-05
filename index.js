f37c4325b97ffded9fc4e983665d19639351c74b
Shiv <shivshive04@yahoo.com>
2018-10-05 12:11:08 +0530

    Updating readme.md

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const HummusRecipe = require('hummus-recipe');
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+var randomstring = require("randomstring");
 
 app.use(bodyParser.json({ limit: '150mb', extended: true }));
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -12,6 +14,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
+app.use(fileUpload());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,30 +22,67 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.post('/pdf/:tagId', (req, res) => {
+    res.set('Content-Type', 'application/force-download');
+    res.download(path.join(__dirname, "public", "PDF", req.params.tagId + ".pdf"));
+});
 app.post('/cpdf', (req, res) => {
     const imagePath = req.body.imageUrl;
     // console.log(imagePath);
 
-    if (fs.existsSync(path.join(__dirname, 'output.pdf'))) {
-        fs.unlinkSync(path.join(__dirname, 'output.pdf'));
+    var pdfFileName = randomstring.generate(7);
+
+    var pdfPath = path.join(__dirname, "public", "PDF", pdfFileName + ".pdf");
+
+    if (fs.existsSync(pdfPath)) {
+        fs.unlinkSync(pdfPath);
     }
+
+    // console.log(req.body.imageUrl);
 
     var b64string = imagePath.replace("data:image/png;base64,", "");
     var buf = Buffer.from(b64string, 'base64');
 
     fs.writeFileSync(path.join(__dirname, 'public', 'message.jpg'), buf);
 
-    const pdfDoc = new HummusRecipe('new', 'output.pdf');
+    const pdfDoc = new HummusRecipe('new', pdfPath);
 
     pdfDoc
         .createPage('letter-size')
-        .image(path.join(__dirname, 'public', 'message.jpg'), 300, 300, { width: 300, keepAspectRatio: true, align: 'center' })
+        .text('Converted Image from Canvas', 'center', 100, {
+            color: '066099',
+            font: "Helvetica",
+            align: "center center",
+            bold: true
+        })
+        .image(path.join(__dirname, 'public', 'message.jpg'), 'center', 200, { width: 600, keepAspectRatio: true, align: 'center' })
         .endPage()
-        .endPDF(()=>{
-            res.download(path.join(__dirname,'output.pdf'));
+        .endPDF(() => {
+
+            var response_obj = { "filePath": pdfFileName }
+            res.send(JSON.stringify(response_obj));
             console.log('END OF SERVER')
         });
 });
+
+
+app.post('/upload', function(req, res) {
+    if (!req.files)
+      return res.status(400).send('No files were uploaded.');
+   
+    let image = req.files.image;
+    console.log(image); 
+    
+    image.mv(path.join(__dirname, 'public', 'images', image.name), function(err) {
+      if (err)
+        return res.status(500).send(err);
+   
+      res.send({
+          uploadedFile: image.name
+      });
+    });
+  });
+
 
 app.listen(3000, () => {
     console.log('application server started....');
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,34 +1,171 @@
 var imageUrl;
+
+var listOfImages = [];
+
+function resizeCanvas(canvas, imageName, callback){
+    listOfImages.push(imageName);
+    var ctx = canvas.getContext("2d");
+    var count = 0;
+    for(var i=0, l= listOfImages.length; i < l; i++){
+        var img = new Image();
+        img.onload = function () {
+            if(ctx.canvas.width < this.width){
+                ctx.canvas.width = this.width;
+            }            
+            ctx.canvas.height += this.height;
+            count++;
+            if(count == l){
+                callback(listOfImages);
+            }
+        }
+        img.src = "images/" + listOfImages[i];
+    }
+}
+
+function drawImage(canvas, imageName){
+    
+    resizeCanvas(canvas, imageName, function(listOfImages){
+        var position = {x :0, y: 0};
+        var ctx = canvas.getContext("2d");
+        for(var i=0, l= listOfImages.length; i < l; i++){
+            var img = new Image();
+            img.onload = function () {
+                ctx.drawImage(this, position.x, position.y, this.width, this.height);
+                position.y += this.height;
+            }
+            img.src = "images/" + listOfImages[i];  
+        }
+    });
+}
+
 $(document).ready(() => {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.imageSmoothingEnabled = 'false';
-    var img = new Image();
+    
+    drawImage(c, "img.png");
+    //creating form
+    var button = $('<input>').attr({
+        class: 'btn btn-primary mb-4',
+        id: 'createPdf',
+        type: 'submit'
+    }).val('Convert To Pdf');
 
-    img.onload = function () {
-        var width_img = this.width;
-        var height_img = this.height;
-        ctx.canvas.width = this.width;
-        ctx.canvas.height = this.height;
-        ctx.drawImage(img, 0, 0, width_img, height_img);
-        c.style.overflow = "scroll";
-    }
-    img.src = "images/test.jpg";
+    var hidden_field = $('<input>').attr({
+        id: 'imageUrl',
+        type: 'hidden',
+        name: 'imageUrl'
+    });
+
+    var form = $('<form>').attr({
+        id: "form_ele",
+        action: "/cpdf",
+        method: "post"
+    }).append(button).append(hidden_field);
+
+    $("#form_cnter").append(form);
 
 });
 
-$('#createPdf').on('click', (eve) => {
 
+$("#canvasholder").on("dragover", function(event){
+    console.log(event);
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+})
+
+
+$("#canvasholder").on("drop", function(event){
+   
+    event.preventDefault();
+    event.stopPropagation();
+
+    var data = event.originalEvent.dataTransfer.files;
+
+    console.log(event, data);
+
+    if(data && data.length > 0){
+        $.each(data, (i, image)=>{
+            var formData = new FormData();
+            // Attach file
+            formData.append('image', image); 
+
+            $.ajax({
+                url: 'upload',
+                data: formData,
+                type: 'POST',
+                contentType: false, // NEEDED, DON'T OMIT THIS (requires jQuery 1.6+)
+                processData: false, // NEEDED, DON'T OMIT THIS,
+                success: function(data){
+                    var c = document.getElementById("myCanvas");
+                    drawImage(c, data.uploadedFile);
+                }
+            });
+        })
+    }
+
+    return false;
+})
+
+
+$(document).on("submit", "#form_ele", (e) => {
+
+    e.preventDefault();
     var can = document.getElementById('myCanvas');
     var ctx = can.getContext('2d');
     imageUrl = can.toDataURL();
+    $('.processing_img').show();
 
-    console.log(imageUrl);
+    // console.log(imageUrl);
     console.log('=----------END-----------=');
+    console.log(imageUrl);
+    $("#imageUrl").val(imageUrl);
 
-    
-    $.AjaxDownloader({
-        url  : "http://127.0.0.1:3000/cpdf",
-        data : {"imageUrl":imageUrl}
+    var frm = $('#form_ele');
+
+    $.ajax({
+        type: frm.attr('method'),
+        url: frm.attr('action'),
+        data: frm.serialize(),
+        success: function (data) {
+            $('.processing_img').hide();
+            console.log(data);
+            var data_response = JSON.parse(data);
+            $.AjaxDownloader({
+                url  : "http://127.0.0.1:3000/pdf/"+data_response.filePath
+            });
+
+        },
+        error: function (data) {
+            console.log('An error occurred.');
+            $('.processing_img').hide();
+
+        },
     });
+
+    // $.AjaxDownloader({
+    //     url  : "http://127.0.0.1:3000/cpdf",
+    //     data : {"imageUrl":imageUrl},
+    // });
+
+
+    // $.AjaxDownloader({
+    //     url  : frm.attr('action'),
+    //     data : frm.serialize(),
+    //     success : function (response) {
+    //         console.log('success');
+    //     }
+    // });
+
+
+    // $.ajax({
+    //     url: "http://127.0.0.1:3000/cpdf",
+    //     type: 'POST',
+    //     data: { "imageUrl": imageUrl },
+    //     success: function (response) {
+    //         $('.processing_img').hide();
+    //         console.log('window file download');
+    //     }
+    // });
 });
